90bea3bb9c73f34fa2dea584ba49699bb6ee8901
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-01-28 16:35:26 -0300

    Define Message.query_params()
    This method parses the user-sent inreach message, returning a dictionary
    of options to be used to fetch a forecast.

--- inreach_ground_control/message.py
+++ inreach_ground_control/message.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, Boolean
 
@@ -41,3 +42,41 @@ class Message(Base):
         Overwrites default magic __repr__ method.
         """
         return f"<Message(text_msg_extid='{self.text_msg_extid}', text_msg='{self.text_msg}', latitude='{self.latitude}', longitude={self.longitude}', response_sent='{self.response_sent}')>"
+
+    def query_params(self):
+        """
+        Parses options contained in the text_msg field (sent from inreach device)
+
+        text_msg is expected to be of the following form
+
+        '''
+        wx <start_time>
+        <opt1>=<val1>
+        <opt2>=<val2>
+        <etc>
+        '''
+
+        This method is case insensitive -- text_msg is downcased before being parsed.
+
+        :return: :class: Query parameters <collections.DefaultDict> object
+        :rtype: collections.DefaultDict
+        """
+        opts = defaultdict(list)
+
+        lines = text_msg.downcase().replace('wx ', '').splitlines()
+
+        start_time = lines[0]
+        if start_time == "now":
+            start_offset_days = 0
+        else:
+            start_offset_days = int(start_time.replace(' days', ''))
+        opts["start_offset_days"] = start_offset_days
+
+        for line in lines:
+            key, value = line.split('=')
+            opts[key] = value
+
+        # "latitude" and "longitude" are required parameters to a weather forecast.
+        # The default values should be supplied by the inreach user's current position.
+        opts["latitude"] = opts["latitude"] or self.latitude
+        opts["longitude"] = opts["longitude"] or self.longitude
