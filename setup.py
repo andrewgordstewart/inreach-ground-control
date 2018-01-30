b4c20139f045514b7d3c8434bbd931fba472d5e8
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-01-30 16:12:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='InreachGroundControl',
     version='0.1.0',
+    description='A command line utility for sending weather reports to the middle of nowhere.',
+    url='https://github.com/andrewgordstewart/inreach-ground-control',
+    author='Andrew Stewart',
+    author_email='andrew.gord.stewart@gmail.com',
+    license='MIT',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
+        'click',
+        'forecastiopy'
     ],
+    python_requires='>=3.6',
     entry_points='''
         [console_scripts]
         weather_report=inreach_ground_control.weather_report:cli
