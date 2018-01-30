380bf89fe7f26165d1a3bdc2bbaf11fb4178fb2c
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-01-30 20:26:27 +0000

    weather_report checks emails first.

--- inreach_ground_control/weather_report.py
+++ inreach_ground_control/weather_report.py
@@ -1,18 +1,29 @@
 import json
 from os import environ
+from datetime import datetime
 
 import click
 
 from inreach_ground_control.database import db_session
+from inreach_ground_control.digest import Digester
 from inreach_ground_control.forecast import Forecast
 from inreach_ground_control.message import Message
 from inreach_ground_control.weatherman import Weatherman
 
 @click.command()
 def cli():
+    log('Checking emails.')
+    Digester(
+        db_session(),
+        host=environ['INREACH_MAIL_HOST'],
+        username=environ['INREACH_MAIL_USERNAME'],
+        password=environ['INREACH_MAIL_PASSWORD']
+    ).check_emails()
     messages = db_session.query(Message).filter(Message.response_sent == False).all()
 
     for message in messages:
+        log('Responding to {message.text_msg_extid} @ {message.latitude}, {message.longitude}')
+
         query_params = message.query_params()
 
         response = Weatherman(message).send_forecast()
@@ -22,5 +33,11 @@ def cli():
             # since inreach.garmin.com returns an html "Error page", along with status code 200.
             message.response_sent = True
             db_session.commit()
+            log('Response sent.')
 
     db_session.remove()
+
+def log(statement):
+    click.echo(f'{datetime.now()} -- {statement}')
+
+    return True
