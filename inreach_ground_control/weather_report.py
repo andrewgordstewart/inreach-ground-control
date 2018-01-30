1a950a49f8a35411af4a829c6af03a7e72f8ab46
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-01-30 22:22:26 +0000

    Remove click dependency

--- inreach_ground_control/weather_report.py
+++ inreach_ground_control/weather_report.py
@@ -1,17 +1,14 @@
 import json
-from os import environ
+from os import environ, path
 from datetime import datetime
 
-import click
+from database import db_session
+from digest import Digester
+from forecast import Forecast
+from message import Message
+from weatherman import Weatherman
 
-from inreach_ground_control.database import db_session
-from inreach_ground_control.digest import Digester
-from inreach_ground_control.forecast import Forecast
-from inreach_ground_control.message import Message
-from inreach_ground_control.weatherman import Weatherman
-
-@click.command()
-def cli():
+def report_weather():
     log('Checking emails.')
     Digester(
         db_session(),
@@ -21,23 +18,31 @@ def cli():
     ).check_emails()
     messages = db_session.query(Message).filter(Message.response_sent == False).all()
 
-    for message in messages:
-        log(f'Responding to {message.text_msg_extid} @ {message.latitude}, {message.longitude}')
+    if environ['INREACH_RESPOND_TO_MESSAGES'] == 'YES':
+        for message in messages:
+            log(f'Responding to {message.text_msg_extid} @ {message.latitude}, {message.longitude}')
 
-        query_params = message.query_params()
+            query_params = message.query_params()
 
-        response = Weatherman(message).send_forecast()
+            response = Weatherman(message).send_forecast()
 
-        if response.status_code == 200 and json.loads(response.text)['Success'] is True:
-            # NB: json.loads may throw an error when the POST request was unsuccessful,
-            # since inreach.garmin.com returns an html "Error page", along with status code 200.
-            message.response_sent = True
-            db_session.commit()
-            log('Response sent.')
+            if response.status_code == 200 and json.loads(response.text)['Success'] is True:
+                # NB: json.loads may throw an error when the POST request was unsuccessful,
+                # since inreach.garmin.com returns an html "Error page", along with status code 200.
+                message.response_sent = True
+                db_session.commit()
+                log('Response sent.')
+    else:
+        log(f'Not responding to new emails.')
 
     db_session.remove()
 
 def log(statement):
-    click.echo(f'{datetime.now()} -- {statement}')
+    filename = f'{path.expanduser("~")}/logs/weather_report.log'
+    with open(filename, 'a+') as f:
+        f.write(f'{datetime.now()} -- {statement}\n')
 
     return True
+
+if __name__ == '__main__':
+    report_weather()
--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     include_package_data=True,
     install_requires=[
         'bs4',
-        'click',
         'datetime',
         'imapclient',
         'forecastiopy',
