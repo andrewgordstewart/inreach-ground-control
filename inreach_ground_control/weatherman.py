e78e685b233f58f1e84ad1f1ef126b7ff448b90a
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-01-31 17:26:23 -0300

    Weatherman allows user to specify units

--- inreach_ground_control/weatherman.py
+++ inreach_ground_control/weatherman.py
@@ -6,13 +6,21 @@ from inreach_ground_control.forecast import Forecast
 REPLY_URL = environ["INREACH_TEXT_MESSAGE_URL"]
 API_KEY = environ["DARK_SKY_API_KEY"]
 
+DEFAULT_DAYS = 3
+DEFAULT_UNITS = 'auto'
+
 class Weatherman():
     def __init__(self, message):
         query_params = message.query_params()
-        self.forecast = Forecast(API_KEY, latitude=query_params['lat'], longitude=query_params['lon'])
+        self.forecast = Forecast(
+            API_KEY,
+            latitude=query_params['lat'],
+            longitude=query_params['lon'],
+            units=query_params['units'] or DEFAULT_UNITS
+        )
 
         report = self.forecast.daily_report(
-            days=query_params['days'] or 3,
+            days=query_params['days'] or DEFAULT_DAYS,
             start_offset_days=query_params['start_offset_days']
         )
 
